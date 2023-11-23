9410b4ea181b640c1d804efbb78a518c60315934
Aleksandr Radchenko <nyataraxis@gmail.com>
2023-11-23 20:28:33 +0300

    [DFT-1] init backend

---
+++ backend/src/test/java/com/nyataraxis/dockerizedfullstacktodo/DockerizedFullstackTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyataraxis.dockerizedfullstacktodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedFullstackTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
